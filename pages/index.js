594dfed6ae8c759c306643997470fe5f5773d09f
Tobias Lehwalder <tlehwalder@posteo.de>
2020-01-22 16:25:38 +0100

    initial commit with already set up project and tailwind

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+import "../styles/index.css";
+
+export default () => (
+  <div className="p-4 shadow rounded bg-gray-700 w-5/6 ">
+    <p className="text-gray-100 text-4xl leading-normal">Next.js Boilerplate</p>
+    <p className="text-gray-500">with Tailwind CSS</p>
+  </div>
+);
